7c5ec1d8d8bc37e94b4b2187e1384d5851b625a1
bubkoo <bubkoo.wy@gmail.com>
2021-03-29 18:55:57 +0800

    feat: ✨ only run with "schedule" event

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import * as core from '@actions/core'
+import { context } from '@actions/github'
 import { Util } from './util'
 import { Issues } from './issues'
 import { Renderer } from './render'
 
 async function run() {
-  const config = Util.getInputs()
-  const timespan = Util.getTimespan()
+  if (context.eventName === 'schedule') {
+    const config = Util.getInputs()
+    const timespan = Util.getTimespan()
 
-  core.info(`Report from ${timespan.fromDate} to ${timespan.toDate}`)
+    core.info(`Report from ${timespan.fromDate} to ${timespan.toDate}`)
 
-  const title = Renderer.renderTitle(timespan, config)
-  const body = await Renderer.renderBody(timespan, config)
-  await Issues.create(title, body, config.addLabels)
+    const title = Renderer.renderTitle(timespan, config)
+    const body = await Renderer.renderBody(timespan, config)
+    await Issues.create(title, body, config.addLabels)
+  }
 }
 
 run()
